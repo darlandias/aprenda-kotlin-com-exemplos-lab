ede5e94442dc99d91853c25ab8585d6cb35ae428
darlandias <125517247+darlandias@users.noreply.github.com>
2023-05-29 11:09:17 -0300

    Evoluindo classes e criando objetos

--- desafio.kt
+++ desafio.kt
@@ -1,24 +1,24 @@
-enum class Nivel {Basico, Intermediario, Avancado}
+enum class Nivel {Nivel1, Nivel2, Nivel3}
 
-data class Usuario(val nome: String, val email: String, val usuario: String)
+data class Aluno(val nome: String, val email: String, val aluno: String)
 
-data class ConteudosEducacionais(val nome: String, val duracao: Int, val nivel: Nivel)
+data class Conteudo(val nome: String, val duracao: Int, val nivel: Nivel)
 
-data class Formacao(val nome: String, val conteudos: List<ConteudosEducacionais>, val nivel: Nivel) {
+data class Formacao(val nome: String, val conteudos: List<Conteudo>, val nivel: Nivel) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf<Aluno>()
     
-    fun matricular(usuario: Usuario) {
-        inscritos.add(usuario)
+    fun matricular(aluno: Aluno) {
+        inscritos.add(aluno)
     }
     
-    fun imprimirListaInscritos(){
+    fun imprimirAlunosInscritos(){
         for(user in inscritos){
             println(user.nome)
         }
     }
     
-    fun imprimirGradeFormacao(){
+    fun imprimirConteudoCurricular(){
         for(cont in conteudos){
             println("${cont.nome} - ${cont.duracao}")
         }
@@ -27,29 +27,29 @@ data class Formacao(val nome: String, val conteudos: List<ConteudosEducacionais>
 
 fun main() {
     
-    val moduloJava = ConteudosEducacionais("Curso de Java", 100, Nivel.Intermediario)
-    val moduloKotlin = ConteudosEducacionais("Curso de Kotlin", 80, Nivel.Intermediario)
-    val moduloDocker = ConteudosEducacionais("Curso de Docker", 50, Nivel.Avancado)
+    val moduloJava = Conteudo("Curso de Java", 100, Nivel.Nivel1)
+    val moduloKotlin = Conteudo("Curso de Kotlin", 80, Nivel.Nivel3)
+    val moduloDocker = Conteudo("Curso de Docker", 50, Nivel.Nivel2)
     
-    val listConteudos = mutableListOf<ConteudosEducacionais>()
+    val listConteudos = mutableListOf<Conteudo>()
     listConteudos.add(moduloJava)
     listConteudos.add(moduloKotlin)
     listConteudos.add(moduloDocker)
     
-    val formacaoAndroid = Formacao("Formação Backend Developer", listConteudos, Nivel.Avancado)
+    val formacao1 = Formacao("Formação Backend Developer", listConteudos, Nivel.Nivel3)
     
-    val Mary = Usuario("Mary", "mary@mail.com", "pro")
-    val Lass = Usuario("Lass", "lass@mail.com", "pro")
-    val Ryan = Usuario("Ryan", "ryan@mail.com", "free")
+    val Mary = Aluno("Mary", "mary@mail.com", "pro")
+    val Lass = Aluno("Lass", "lass@mail.com", "pro")
+    val Ryan = Aluno("Ryan", "ryan@mail.com", "free")
     
-    formacaoAndroid.matricular(Lass)
-    formacaoAndroid.matricular(Ryan)
+    formacao1.matricular(Lass)
+    formacao1.matricular(Ryan)
     
     print("Formação: ")
-    println("${formacaoAndroid.nome} - ${formacaoAndroid.nivel}")
+    println("${formacao1.nome} - ${formacao1.nivel}")
     println("Disciplinas propostas: ")
-    println(formacaoAndroid.imprimirGradeFormacao())
+    println(formacao1.imprimirConteudoCurricular())
     println()
     println("Lista de inscritos aceitos: ")
-    println(formacaoAndroid.imprimirListaInscritos())
+    println(formacao1.imprimirAlunosInscritos())
 }
